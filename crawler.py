f343c0643e6f6f6e93bbc5399e5fe58e4ba09213
Dai Vuong <daivuong7696@gmail.com>
2018-08-30 16:53:25 +0700

    Add photo-downloading-from-tag feature

--- crawler.py
+++ crawler.py
@@ -1,11 +1,70 @@
-from __future__ import unicode_literals
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import ( division, absolute_import, print_function, unicode_literals )
 from builtins import open
 
 from inscrawler import InsCrawler
 import sys
 import argparse
 import json
+import pandas as pd
 from io import open
+import operator
+
+import sys, os, tempfile, logging
+
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+else:
+    import urllib2
+    import urlparse
+
+def download_file(url, dest=None, filename=None):
+    """ 
+    Download and save a file specified by url to dest directory,
+    """
+    try: 
+        u = urllib2.urlopen(url)
+    except urllib2.URLError as e:
+        print('{} is expired'.format(filename))
+        pass
+    else:
+        scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+        #filename = os.path.basename(path)
+        if not filename:
+            filename = 'downloaded.file'
+        if dest:
+            filename = os.path.join(dest, filename)
+
+        with open(filename, 'wb') as f:
+            meta = u.info()
+            meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
+            meta_length = meta_func("Content-Length")
+            file_size = None
+            if meta_length:
+                file_size = int(meta_length[0])
+            print("Downloading: {0} Bytes: {1}".format(url, file_size))
+
+            file_size_dl = 0
+            block_sz = 8192
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+
+                file_size_dl += len(buffer)
+                f.write(buffer)
+
+                status = "{0:16}".format(file_size_dl)
+                if file_size:
+                    status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
+                status += chr(13)
+                print(status, end="")
+            print()
+
+    return filename
 
 
 def usage():
@@ -41,6 +100,21 @@ def arg_required(args, fields=[]):
             sys.exit()
 
 
+def download_photo(data, filepath, tag):
+    out = json.dumps(data, ensure_ascii=False)
+    df = pd.read_json(out)
+
+    df['file_name'] = (df['img_url']
+                        .str.rsplit('/', n=1, expand=True)
+                        .iloc[:, 1]
+                      )
+    directory = os.path.join(filepath, tag)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i in range(len(df)):
+        download_file(df['img_url'][i], dest=directory, filename=df['file_name'][i])
+    
 def output(data, filepath):
     out = json.dumps(data, ensure_ascii=False)
     if filepath:
@@ -49,12 +123,11 @@ def output(data, filepath):
     else:
         print(out)
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Instagram Crawler',
                                      usage=usage())
     parser.add_argument('mode',
-                        help='options: [posts, posts_full, profile, hashtag]')
+                        help='options: [posts, posts_full, profile, hashtag, photo-hashtag]')
     parser.add_argument('-n', '--number',
                         type=int,
                         help='number of returned posts')
@@ -79,6 +152,10 @@ if __name__ == '__main__':
     elif args.mode == 'profile':
         arg_required('username')
         output(get_profile(args.username), args.output)
+    elif args.mode == 'photo-hashtag':
+        arg_required('tag')
+        download_photo(
+            get_posts_by_hashtag(args.tag, args.number or 100), args.output, args.tag)
     elif args.mode == 'hashtag':
         arg_required('tag')
         output(
